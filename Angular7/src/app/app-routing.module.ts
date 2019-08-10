c2f2bddb05dac0db345a28a174ae7a41893c8733
yaseen <yaseen.patel2005@hotmail.com>
2019-08-10 19:04:52 +0100

    Started Angular and sorting routes

---
+++ Angular7/src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {Routes, RouterModule } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { RegistrationComponent } from './user/registration/registration.component';
+
+
+// The below is how we set routes for angular projects e.g: /user/registration
+const routes: Routes = [
+  { path:'', redirectTo:'/user/registration', pathMatch:'full'},
+
+  { path:'user', component: UserComponent, 
+  children: [
+    {path: 'registration', component: RegistrationComponent }
+    ] 
+  }
+];
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+
+})
+export class AppRoutingModule { }
---
+++ Angular7/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { RegistrationComponent } from './user/registration/registration.component';
+import { AppRoutingModule } from './app-routing.module';
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    RegistrationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
